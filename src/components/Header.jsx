2b40035a9332be4da53a1d8f463f4cfae0ebf414
Awais Isane <awaisisane@gmail.com>
2021-09-26 22:32:03 +0530

    Created all pages

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,12 @@
 import  React from 'react';
 import { Appbar } from 'react-native-paper';
 
-const Header = ({goBack}) => {
+const Header = ({goBack,dis}) => {
 
 
   return (
     <Appbar.Header>
-      <Appbar.BackAction onPress={goBack} />
+      <Appbar.BackAction onPress={goBack} disabled={dis}/>
       <Appbar.Content title="COVID-STATS" subtitle="covid stats app" />
 
     </Appbar.Header>
